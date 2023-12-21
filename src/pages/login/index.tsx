8590ed86f030d1024166121564bb48907c52b202
slog-developer <amisgyeong@gmail.com>
2023-12-21 23:10:59 +0900

    컴포넌트만들기

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,5 @@
+export default function LoginPage() {
+  return (
+    <h4>Login Page</h4>
+  )
+}
---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,5 @@
+export default function SignupPage() {
+  return (
+    <h4>Signin Page</h4>
+  )
+}
