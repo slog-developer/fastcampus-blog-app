8376a3743a1bd8daa521224df7fb36db0a52f6c0
slog-developer <amisgyeong@gmail.com>
2024-01-01 10:10:36 +0900

    사용자 인증구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { app } from "firebaseApp";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import Router from "./components/Router";
+import Loader from "components/Loader";
 
 function App() {
   const auth = getAuth(app);
-  const [isAuthenticated, setInAuthenticated] = useState<boolean>(
+
+  // auth를 체크하기 전에 (initialize)전에는 loader를 띄워주는 용도
+  const [init, setInit] = useState<boolean>(false);
+  // auth의 currentUser가 있으면 authenticated로 변경
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
     !!auth?.currentUser
   );
 
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+      setInit(true);
+    });
+  }, [auth]);
   return (
     <>
       <ToastContainer />
-      <Router isAuthenticated={isAuthenticated} />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
     </>
   );
 }
